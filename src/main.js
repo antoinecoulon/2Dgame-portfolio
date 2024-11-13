8d48d6dcc07d2420f4829d8ba3c4f0286f0fd57f
Antoine Coulon <a.antoinecoulon@gmail.com>
2024-11-13 23:29:22 +0100

    Create player object

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { SCALE_FACTOR } from "./constants";
 import { k } from "./kaboomCtx";
 
-// Charger les sprites loadSprite( name , file , { properties to slice the image }), les fichiers de 'public' sont directement accessible grâce à Vite
+/**
+ * Charger les sprites loadSprite( name , file , { properties to slice the image }), les fichiers de 'public' sont directement accessible grâce à Vite
+ * .loadSprite([name], [file], {parameters})
+ * int target spritesheets sprites references
+ */
 k.loadSprite("spritesheet", "./spritesheet.png", {
     sliceX: 39,
     sliceY: 31,
@@ -28,13 +32,30 @@ k.scene("main", async () => {
     const mapData = await(await fetch("./map.json")).json();
     const layers = mapData.layers;
 
-    const map = k.make([
-        k.sprite("map"),
-        k.pos(0),
+    const map = k.make([            // .make([]) create a (kaboom) object NOT displaying it
+        k.sprite("map"),            // from .loadSprite() (l.18)
+        k.pos(0),                   // position where the object is drawn
+        k.scale(SCALE_FACTOR)      // the scale of the object (here x4, see ./constants)
+    ]);
+
+    const player = k.make([
+        k.sprite("spritesheet", { anim: "idle-down" }), // anim parameter is default animation of the sprite
+        k.area({                                        // .area({ [parameters] }) : hitbox of this player
+            shape: new k.Rect(vec2(0, 3), 10, 10),      // plays with hitbox position X and Y values, and the width, height values
+        }),
+        k.body(),                                       // activate collision for this object
+        k.anchor("center"),                             // X, Y coordinates from where the object is drawn
+        k.pos(),                                        // TO DO player spawn 
         k.scale(SCALE_FACTOR),
+        {                                               // create an object inside the array. Properties are accessible with .propertie attributes
+            speed: 250,
+            direction: "down",
+            isInDialogue: false,
+        },
+        "player"
     ]);
 
 });
 
-// .go(scene name) which scene go with the app launch, using main.js as entrance point
+// .go([scene name]) which scene go with the app launch, using main.js as entrance point
 k.go("main");
