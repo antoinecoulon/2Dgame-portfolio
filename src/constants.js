8b6d7304a7f8cdd0f9d0f428f49c3c09962e841a
Antoine Coulon <a.antoinecoulon@gmail.com>
2024-11-24 15:44:23 +0100

    Feature: implement onbjetcs interactions. ToDo: dialogue texts and links

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,14 @@
 export const SCALE_FACTOR = 4;
 
 export const dialogueData = {
-    pc: '',
-    tv: '',
-    bed: '',
-    resume: ''
+    pc: '',                 // ToDo passion pour l'informatique
+    tv: '',                 // ...
+    bed: '',                // ...
+    resume: '',             // lien vers CV
+    projetcs: '',           // lien vers GitHub
+    'cs-degree': '',        // diplome
+    library: '',            // a propos de mes lectures
+    'sofa-table': '',       // take a break 
+    exit: ''                // si vous voulez déjà me quitter vous 
+                            // pouvez fermer l'onglet du navigateur... Merci et au revoir
 }
