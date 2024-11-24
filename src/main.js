8e36cc39a6c0dd8f660213861f7f3cfca82798bf
Antoine Coulon <a.antoinecoulon@gmail.com>
2024-11-24 15:29:42 +0100

    Fix: animations now stop

--- src/main.js
+++ src/main.js
@@ -152,6 +152,19 @@ k.scene("main", async () => {
         }
     });
 
+    k.onMouseRelease(() => {
+        if (player.direction === "down") {
+            player.play("idle-down");
+            return;
+        }
+        if (player.direction === "up") {
+            player.play("idle-up");
+            return;
+        }
+
+        player.play("idle-side");
+    });
+
 });
 
 // .go([scene name]) which scene go with the app launch, using main.js as entrance point
