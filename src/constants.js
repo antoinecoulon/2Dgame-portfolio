8ed7f48c1d1878701b173ee81a8774e7263f3337
Antoine Coulon <a.antoinecoulon@gmail.com>
2024-11-30 08:51:27 +0100

    Add: dialogs

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,13 @@
 export const SCALE_FACTOR = 4;
 
 export const dialogueData = {
-    pc: '',                 // ToDo passion pour l'informatique
-    tv: '',                 // ...
-    bed: '',                // ...
+    pc: 'Mon PC est mon poste de travail, mais j\'aime aussi jouer à des jeux vidéos ! Vous pouvez me contacter par mail à cette adresse: a.antoinecoulon@gmail.com',
+    tv: 'De bons films ou séries à me conseiller ??',
+    bed: 'Un repos bien mérité...',
     resume: '',             // lien vers CV
-    projetcs: 'Vous pourrez trouver mes projets personnels sur mon <a href="https://https://github.com/antoinecoulon target="_blank">">GitHub</a> ! N\'hésitez pas à aller voir et à me dire ce que vous en pensez.',           // lien vers GitHub
+    projetcs: 'Vous pourrez trouver mes projets personnels sur mon <a href="https://https://github.com/antoinecoulon target="_blank">">GitHub</a> ! N\'hésitez pas à aller voir et à me dire ce que vous en pensez.',
     'cs-degree': '',        // diplome
-    library: '',            // a propos de mes lectures
-    'sofa-table': '',       // take a break 
-    exit: ''                // si vous voulez déjà me quitter vous 
-                            // pouvez fermer l'onglet du navigateur... Merci et au revoir
+    library: 'J\'aime énormément lire ! Je suis aussi issu d\'une formation littéraire.',
+    'sofa-table': 'En général, ce sont mes chats qui se trouvent là...',
+    exit: 'J\'espère que cette visite vous a plu, et j\'ai hâte de vous revoir! See you soon!'
 }
