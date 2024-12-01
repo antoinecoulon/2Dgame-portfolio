6090f30873befb0e87a5f7925ec9f4b29a028bf9
Antoine Coulon <a.antoinecoulon@gmail.com>
2024-12-01 08:37:35 +0100

    Add: resume

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ export const dialogueData = {
     pc: 'Mon PC est mon poste de travail, mais j\'aime aussi jouer à des jeux vidéos ! Vous pouvez me contacter par mail à cette adresse: a.antoinecoulon@gmail.com',
     tv: 'Je vous donne le lien de <a href="https://www.youtube.com/watch?v=wy_fSStEgMs&t=499s&ab_channel=freeCodeCamp.org" target="_blank">la vidéo</a> qui m\'a guidé dans la création de cette application, merci à JSLegendDev ! J\'ai trouvé l\'idée super intéressante.',
     bed: 'Un repos bien mérité...',
-    resume: 'test',             // lien vers CV
+    resume: 'Vous pouvez consulter <a href="./public/CV_AntoineCoulon_censured.pdf" target="_blank">mon CV</a> ici !',
     projects: 'Vous pourrez trouver mes projets personnels sur mon <a href="https://github.com/antoinecoulon" target="_blank">GitHub</a> ! N\'hésitez pas à aller voir et à me dire ce que vous en pensez.',
     'cs-degree': 'test',        // diplome
     library: 'J\'aime énormément lire ! Je suis aussi issu d\'une formation littéraire.',
